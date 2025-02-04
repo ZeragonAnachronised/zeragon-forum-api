65feaf90e4467736742c42482198cb4798eeadbf
ZeragonAnachronised <ioanidiilya@gmail.com>
2025-02-04 17:04:14 +0600

    new custom validation errors

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,16 @@ class UserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|string|max:255|min:6'
+        ], [
+            'name.required' => 'Вы не ввели имя',
+            'name.max' => 'Имя слишком длинное',
+            'email.required' => 'Вы не ввели email',
+            'email.max' => 'Email слишком длинный',
+            'email.email' => 'Неверно введён email',
+            'email.unique' => 'Email уже занят',
+            'password.required' => 'Вы не ввели пароль',
+            'password.max' => 'Пароль слишком длинный',
+            'password.min' => 'Пароль должен быть от 6 символов'
         ]);
 
         $random_num = dechex(rand(1, 9999));
@@ -29,17 +39,30 @@ class UserController extends Controller
         $user->save();
 
         return response()->json([
-            'user' => $user
+            'user' => $user,
+            'success' => true
         ], 201);
     }
 
     public function login(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|max:255|min:6'
+        ], [
+            'email.required' => 'Вы не ввели email',
+            'email.max' => 'Email слишком длинный',
+            'email.email' => 'Неверно введён email',
+            'password.required' => 'Вы не ввели пароль',
+            'password.max' => 'Пароль слишком длинный',
+            'password.min' => 'Пароль должен быть от 6 символов'
+        ]);
         $credentials = $request->only('email', 'password');
 
         if (!$token = JWTAuth::attempt($credentials)) {
             return response()->json([
-                'error' => 'Unauthorized'
+                'success' => false,
+                'message' => 'Неверные данные'
             ], 401);
         }
 
